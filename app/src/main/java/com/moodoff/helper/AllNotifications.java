3795d1f4580590acd0b4d4003484bf00bfbb1ab3
snaskar <santanu.code@gmail.com>
2016-11-13 16:36:31 +0530

    Made generic mood work with different moods, increased performance on startup.

---
+++ app/src/main/java/com/moodoff/helper/AllNotifications.java
@@ -0,0 +1,13 @@
+package com.moodoff.helper;
+
+import java.util.ArrayList;
+
+/**
+ * Created by snaskar on 11/12/2016.
+ */
+
+public class AllNotifications {
+    //public static String allNotifications="";
+    public static int totalNoOfNot = 0;
+    public static ArrayList<String> allNotifications;
+}
